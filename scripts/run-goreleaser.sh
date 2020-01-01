2b33e93f8591fbcefd0f096d6ca6ffe16eb4ea74
Sandro Mello <sandromll@gmail.com>
2020-01-01 18:44:33 -0300

    Fix permission denied problem when extracing goreleaser Extract goreleaser binary in /tmp folder

--- scripts/run-goreleaser.sh
+++ scripts/run-goreleaser.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ -f "/usr/local/bin/goreleaser" ]; then
-    "/usr/local/bin/goreleaser" "$@"
-	exit $?
-fi
-
 VERSION=v0.123.3
 CHECKSUM=cad997014e5c6a462488757087db4145c2ae7d7d73a29cb62bbfd41f18ccea30
 TAR_FILE="/tmp/goreleaser_$(uname -s)_$(uname -m).tar.gz"
@@ -14,5 +9,5 @@ RELEASES_URL="https://github.com/goreleaser/goreleaser/releases"
 curl -o "$TAR_FILE" -fSL "https://github.com/goreleaser/goreleaser/releases/download/${VERSION}/goreleaser_$(uname -s)_$(uname -m).tar.gz"; \
 	echo "${CHECKSUM}  ${TAR_FILE}" | shasum -c -;
 
-tar -xf "$TAR_FILE" -C /usr/local/bin/ goreleaser && rm -f $TAR_FILE
-"/usr/local/bin/goreleaser" "$@"
+tar -xf "$TAR_FILE" -C /tmp/ goreleaser && rm -f $TAR_FILE
+"/tmp/goreleaser" "$@"
